bf011515eb79400601e07979771fc63482287c22
James Serengia <jamesserengia@gmail.com>
2023-05-04 05:11:34 +0300

    Revert "Refactored views using partials"
    This reverts commit d1cb6eca4904a39118092d19a5a99d4e7926ddbb.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
-
   def show
+    @article = Article.find(params[:id])
   end
 
   def index
@@ -24,9 +23,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article successfully updated!"
       redirect_to @article
@@ -36,15 +37,12 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
   private
-
-  def set_article
-    @article = Article.find(params[:id])
-  end
 
   def article_params
     params.require(:article).permit(:title, :description)
